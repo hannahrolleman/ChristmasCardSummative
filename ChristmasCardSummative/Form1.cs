349821f76cbafc7db50062988f9286805ec3895c
hannahrolleman <hannroll516@ed.amdsb.ca>
2017-09-22 13:09:45 -0400

    Add project files.

---
+++ ChristmasCardSummative/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChristmasCardSummative
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            BackgroundImage = null;
+            Refresh();
+
+            Graphics formGraphics = this.CreateGraphics();
+            SolidBrush drawBrush = new SolidBrush(Color.White);
+
+            BackColor = Color.CadetBlue;
+            formGraphics.FillEllipse(drawBrush, 25, 25, 200, 100);
+
+        }
+    }
+}
