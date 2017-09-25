54dd6fbf25acc45d5fdf3fa9ccb3665adc5e4c13
hannahrolleman <hannroll516@ed.amdsb.ca>
2017-09-25 13:30:13 -0400

    added snow

--- ChristmasCardSummative/Form1.cs
+++ ChristmasCardSummative/Form1.cs
@@ -26,8 +26,35 @@ namespace ChristmasCardSummative
             SolidBrush drawBrush = new SolidBrush(Color.White);
 
             BackColor = Color.CadetBlue;
-            formGraphics.FillEllipse(drawBrush, 25, 25, 200, 100);
+            formGraphics.FillEllipse(drawBrush, 223, 125, 15, 15);
+            formGraphics.FillEllipse(drawBrush, 156, 167, 15, 15);
+            formGraphics.FillEllipse(drawBrush, 98, 116, 15, 15);
+            formGraphics.FillEllipse(drawBrush, 244, 206, 15, 15);
+            formGraphics.FillEllipse(drawBrush, 184, 246, 15, 15);
+            formGraphics.FillEllipse(drawBrush, 241, 40, 15, 15);
+            formGraphics.FillEllipse(drawBrush, 325, 265, 15, 15);
+            formGraphics.FillEllipse(drawBrush, 46, 48, 15, 15);
+            formGraphics.FillEllipse(drawBrush, 84, 95, 15, 15);
+            formGraphics.FillEllipse(drawBrush, 116, 196, 15, 15);
+            formGraphics.FillEllipse(drawBrush, 359, 171, 15, 15);
+            formGraphics.FillEllipse(drawBrush, 103, 133, 15, 15);
+            formGraphics.FillEllipse(drawBrush, 45, 206, 15, 15);
+            formGraphics.FillEllipse(drawBrush, 164, 61, 15, 15);
+            formGraphics.FillEllipse(drawBrush, 55, 106, 15, 15);
+            formGraphics.FillEllipse(drawBrush, 185, 160, 15, 15);
+            formGraphics.FillEllipse(drawBrush, 132, 84, 15, 15);
+            formGraphics.FillEllipse(drawBrush, 99, 288, 15, 15);
+            formGraphics.FillEllipse(drawBrush, 50, 86, 15, 15);
+            formGraphics.FillEllipse(drawBrush, 333, 192, 15, 15);
+            formGraphics.FillEllipse(drawBrush, 149, 78, 15, 15);
+            formGraphics.FillEllipse(drawBrush, 287, 207, 15, 15);
+            formGraphics.FillEllipse(drawBrush, 323, 83, 15, 15);
+            formGraphics.FillEllipse(drawBrush, 300, 164, 15, 15);
+            formGraphics.FillEllipse(drawBrush, 269, 179, 15, 15);
+            formGraphics.FillEllipse(drawBrush, 133, 32, 15, 15);
+
 
         }
     }
+    
 }
